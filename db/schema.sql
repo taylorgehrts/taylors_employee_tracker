523e4436a0686da3fd7f2b6007f09e1fa20642f5
taylorgehrts <taylorgehrts@gmail.com>
2023-07-26 14:12:01 -0700

    created schema file and created tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,28 @@
---create schema here
 DROP DATABASE IF EXISTS employees_DB;
 CREATE DATABASE employees_DB;
+USE employees_DB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department (id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
